b9e86429bd5fc1420d9e2dc0768a408bb08aa20f
SebastianHernandez69 <sh3641046@gmail.com>
2025-04-25 10:01:57 -0600

    Adding select_all_reports

--- controllers/PokeRequestController.py
+++ controllers/PokeRequestController.py
@@ -5,6 +5,7 @@ from fastapi import HTTPException
 from models.PokeRequest import PokeRequest
 from utils.db import execute_query_json
 from utils.AzQueue import AzQueue
+from utils.AzBlob import AzBlob
 
 #logging configuration
 logging.basicConfig(level=logging.INFO)
@@ -47,3 +48,25 @@ async def insert_poke_request(poke_request: PokeRequest) -> dict:
     except Exception as e:
         logger.error(f"Error inserting poke request: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+
+async def get_all_request() -> dict:
+    query = """
+        select 
+            r.id as ReportId
+            , s.description as Status
+            , r.type as PokemonType
+            , r.url 
+            , r.created 
+            , r.updated
+        from pokequeue.requests r 
+        inner join pokequeue.status s 
+        on r.id_status = s.id 
+    """
+    result = await execute_query_json( query  )
+    result_dict = json.loads(result)
+    blob = AzBlob()
+    for record in result_dict:
+        id = record['ReportId']
+        record['url'] = f"{record['url']}?{blob.generate_sas(id)}"
+    return result_dict
--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import uvicorn
 import json
 from fastapi import FastAPI
 from utils.db import execute_query_json
-from controllers.PokeRequestController import insert_poke_request, update_poke_request, get_poke_request
+from controllers.PokeRequestController import insert_poke_request, update_poke_request, get_poke_request, get_all_request
 from models.PokeRequest import PokeRequest
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 @app.get("/")
 async def root():
@@ -22,6 +31,10 @@ async def version():
 @app.get("/api/request/{id}")
 async def get_request(id: int):
     return await get_poke_request( id )
+
+@app.get("/api/request")
+async def select_all_request():
+    return await get_all_request( )
 
 @app.post("/api/request")
 async def create_request(poke_request: PokeRequest):
